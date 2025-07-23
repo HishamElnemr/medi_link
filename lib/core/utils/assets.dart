ada8ab7dbef23f9f54078276fb5045f14b36370c
Hisham Elnemr <hishammelnmr@gmail.com>
2025-07-24 01:48:23 +0300

    first commit

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesAppLogo
+  /// assets/images/app_logo.jpeg
+  static const String assetsImagesAppLogo = "assets/images/app_logo.jpeg";
+
+  /// Assets for assetsImagesAppLogoRemovebg
+  /// assets/images/app_logo-removebg.png
+  static const String assetsImagesAppLogoRemovebg = "assets/images/app_logo-removebg.png";
+}
+
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MediLink());
+}
+
+class MediLink extends StatelessWidget {
+  const MediLink({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Cairo',
+        scaffoldBackgroundColor: Colors.white,
+      ),
+    );
+  }
+}
